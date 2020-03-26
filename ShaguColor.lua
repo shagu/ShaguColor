b4fd1b82b665c36ea6ef5879ec3707d34585bc62
shagu <ericmauser@googlemail.com>
2020-03-26 15:50:16 +0100

    shagucolor: add prefix comment and clean up code

--- ShaguColor.lua
+++ ShaguColor.lua
@@ -1,28 +1,23 @@
-function ShaguColor () 
-    function ColorPickerFrame.func (r,g,b,a)
-        local r,g,b, _ = ColorPickerFrame:GetColorRGB()
-        local hex = string.format("%02x%02x%02x", r*255, g*255, b*255)
-        if(not scm) then 
-            scm = SendChatMessage; 
-        end; 
-    
-        function SendChatMessage(msg, type, lang, chan) 
-            scm("\124cff" .. hex .. "\124Hitem:19:0:0:0:0:0:0:0\124h" ..msg.."\124h\124r", type, lang, chan);
-        end;
-    end
+local scm
+-- some servers require a whitespace prefix in front of items
+-- /run SCPREFIX=" "
+SCPREFIX = ""
 
-    function ColorPickerFrame.cancelFunc ()
-        if(not scm) then 
-            scm = SendChatMessage; 
-        end; 
-        
-        function SendChatMessage(msg,type,lang,chan) 
-            scm(msg,type,lang,chan);
-        end;
+function ShaguColor ()
+  function ColorPickerFrame.func (r,g,b,a)
+    local r,g,b, _ = ColorPickerFrame:GetColorRGB()
+    local hex = string.format("%02x%02x%02x", r*255, g*255, b*255)
+    scm = scm or SendChatMessage
+    function SendChatMessage(msg, type, lang, chan)
+      scm(SCPREFIX.."|cff" .. hex .. "|Hitem:6948:0:0:0|h" .. msg .. "|h|r", type, lang, chan);
     end
+  end
 
-    ColorPickerFrame:Show();
-    ColorPickerFrame:Show();
+  function ColorPickerFrame.cancelFunc ()
+    SendChatMessage = scm or SendChatMessage
+  end
+
+  ColorPickerFrame:Show();
 end
 
 SLASH_SHAGUCOLOR1, SLASH_SHAGUCOLOR2 = "/scol", "/shagucolor"
